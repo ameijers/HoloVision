cb285210548d8cea65c05feee963a37cb2ae60a3
Alexander Meijers <alexander@appzinside.com>
2018-01-06 20:00:50 +0100

    Add files via upload

---
+++ HoloVisionAPI/HoloVisionAPI/ConfigSettingscs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoloVisionAPI
+{
+    public class ConfigSettingscs
+    {
+        public static string VisionKey = "71b4a33b6f4647c693d1843f2066ab30";
+
+        public static string VisionUrlParams = "visualFeatures=Tags";
+        public static string VisionURL = string.Format("https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze?{0}", VisionUrlParams);
+    }
+}
---
+++ HoloVisionAPI/HoloVisionAPI/VisionAPI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace HoloVisionAPI
+{
+    public delegate void OnGetDataCompleted(string id, string json);
+
+    public class VisionAPI : System.Object
+    {
+        public void GetDataAsync(string id, string url, byte[] body, string key, OnGetDataCompleted handler)
+        {
+
+        IAsyncAction asyncAction = Windows.System.Threading.ThreadPool.RunAsync(
+            async (workItem) =>
+            {
+                try
+                {
+                    WebRequest webRequest = WebRequest.Create(url);
+                    webRequest.Method = "POST";
+                    webRequest.Headers["Content-Type"] = "application/octet-stream";
+                    webRequest.Headers["Ocp-Apim-Subscription-Key"] = key;
+
+                    Stream stream = await webRequest.GetRequestStreamAsync();
+                    stream.Write(body, 0, body.Length);
+
+                    WebResponse response = await webRequest.GetResponseAsync();
+ 
+                    Stream result = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(result);
+ 
+                    string json = reader.ReadToEnd();
+ 
+                    handler(id, json);
+                }
+                catch (Exception ex)
+                {
+                    // handle errors
+                }
+            }
+            );
+
+        asyncAction.Completed = new AsyncActionCompletedHandler(GetDataAsyncCompleted);
+        }
+
+    private void GetDataAsyncCompleted(IAsyncAction asyncInfo, AsyncStatus asyncStatus)
+    {
+    }
+
+    }
+}
